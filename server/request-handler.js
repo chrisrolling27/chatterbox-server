f234382da6c2630dff29687a0095a9a591c3612b
ChrisRolling <chrisrolling27@gmail.com>
2021-12-04 13:35:28 -0800

    assignment complete

--- server/request-handler.js
+++ server/request-handler.js
@@ -21,7 +21,9 @@ this file and include it in basic-server.js so that it actually works.
 // which is considered a different domain.
 //
 // Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
+// client from this domain by setting up static file serving
+
+
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
@@ -35,7 +37,7 @@ var testData = [
     'message_id': 43664,
     'roomname': 'lobby',
     'text': 'neato!',
-    'username': 'anonymous',
+    'username': 'a0',
     'github_handle': 'beezymc',
     'campus': 'hr-sfo',
     'created_at': '2021-12-01T01:16:39.363Z',
@@ -45,7 +47,7 @@ var testData = [
     'message_id': 43663,
     'roomname': 'lobby',
     'text': 'cool!',
-    'username': 'anonymous',
+    'username': 'a1',
     'github_handle': 'beezymc',
     'campus': 'hr-sfo',
     'created_at': '2021-12-01T01:07:48.865Z',
@@ -53,9 +55,8 @@ var testData = [
   }
 ];
 
-
-var requestHandler = function(request, response) {
-  var { headers, method, url} = request;
+var requestHandler = function (request, response) {
+  var { headers, method, url } = request;
   // how to respond to GET
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
@@ -64,100 +65,89 @@ var requestHandler = function(request, response) {
   headers['Content-Type'] = 'text/plain';
 
 
-  if (url !== '/classes/messages') {
-    console.log('bad URL!');
+  if (url === '/classes/messages') {
+
+    if (method === 'OPTIONS') {
+      var statusCode = 200;
+      response.writeHead(statusCode, headers);
+      response.end();
+    }
+
+    if (method === 'GET') {
+      var statusCode = 200;
+      response.writeHead(statusCode, headers);
+      response.end(JSON.stringify(testData));
+    }
+
+    // how to respond to POST
+    if (method === 'POST') {
+      var statusCode = 201;
+      var data;
+      request.on('data', (chunk) => { // chunk is a stringified object message
+        data = JSON.parse(chunk);
+
+        testData.unshift(data);
+        console.log(testData);
+        response.writeHead(statusCode, headers);
+        response.end();
+
+      });
+    }
+
+  } else {
+
+    console.log('bad URL!!');
     var statusCode = 404;
     response.writeHead(statusCode, headers);
     response.end(); //does this really end things?
+
   }
 
-  if (method === 'OPTIONS') {
-    var statusCode = 200;
-    response.writeHead(statusCode, headers);
-    response.end();
-    // response.end(JSON.stringify(testData));
-  }
 
-  if (method === 'GET') {
-    var statusCode = 200;
-    response.writeHead(statusCode, headers);
-    response.end(JSON.stringify(testData));
-  }
-
-  // how to respond to POST
-  if (method === 'POST') {
-    var statusCode = 201;
-    var data;
-    request.on('data', (chunk) => { // chunk is a stringified object message
-      data = JSON.parse(chunk);
-
-
-      // var dataRequirementSatisfied = true;
-      // var requiredFields = ['text', 'username'];
-
-      // for (var field of requiredFields) {
-      //   if (data[field] === undefined) {
-      //     dataRequirementSatisfied = false;
-      //   }
-      // }
-
-      // if (!dataRequirementSatisfied) {
-      //   var statusCode = 404;
-      //   console.log('FAILURE: no username');
-      //   response.writeHead(statusCode, headers);
-      //   response.end();
-      // } else {
-
-      testData.unshift(data);
-      response.writeHead(statusCode, headers);
-      response.end(JSON.stringify(testData));
-
-    });
-  }
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // // The outgoing status.
-  // var statusCode = 200;
-
-  // // See the note below about CORS headers.
-  // var headers = defaultCorsHeaders;
-
-  // // Tell the client we are sending them plain text.
-  // //
-  // // You will need to change this if you are sending something
-  // // other than plain text, like JSON or HTML.
-  // headers['Content-Type'] = 'text/plain';
-
-  // // .writeHead() writes to the request line and headers of the response,
-  // // which includes the status and all headers.
-  // response.writeHead(statusCode, headers);
-
-  // // Make sure to always call response.end() - Node may not send
-  // // anything back to the client until you do. The string you pass to
-  // // response.end() will be the body of the response - i.e. what shows
-  // // up in the browser.
-  // //
-  // // Calling .end 'flushes' the response's internal buffer, forcing
-  // // node to actually send all the data over to the client.
-  // response.end('Hello, World!!!');
 };
 
 
-
-
-
 exports.requestHandler = requestHandler;
+
+
+
+// Request and Response come from node's http module.
+//
+// They include information about both the incoming request, such as
+// headers and URL, and about the outgoing response, such as its status
+// and content.
+//
+// Documentation for both request and response can be found in the HTTP section at
+// http://nodejs.org/documentation/api/
+
+// Do some basic logging.
+//
+// Adding more logging to your server can be an easy way to get passive
+// debugging help, but you should always be careful about leaving stray
+// console.logs in your code.
+
+
+// // The outgoing status.
+// var statusCode = 200;
+
+// // See the note below about CORS headers.
+// var headers = defaultCorsHeaders;
+
+// // Tell the client we are sending them plain text.
+// //
+// // You will need to change this if you are sending something
+// // other than plain text, like JSON or HTML.
+// headers['Content-Type'] = 'text/plain';
+
+// // .writeHead() writes to the request line and headers of the response,
+// // which includes the status and all headers.
+// response.writeHead(statusCode, headers);
+
+// // Make sure to always call response.end() - Node may not send
+// // anything back to the client until you do. The string you pass to
+// // response.end() will be the body of the response - i.e. what shows
+// // up in the browser.
+// //
+// // Calling .end 'flushes' the response's internal buffer, forcing
+// // node to actually send all the data over to the client.
+// response.end('Hello, World!!!');
--- server/spec/ServerSpec.js
+++ server/spec/ServerSpec.js
@@ -57,6 +57,7 @@ describe('Node Server Request Listener Function', function() {
 
   it('Should respond with messages that were previously posted', function() {
     var stubMsg = {
+      username: 'Jono',
       text: 'Do my bidding!'
     };
     var req = new stubs.request('/classes/messages', 'POST', stubMsg);
@@ -89,46 +90,5 @@ describe('Node Server Request Listener Function', function() {
     expect(res._responseCode).to.equal(404);
     expect(res._ended).to.equal(true);
   });
-
-
-  // it('Should 404 when POST does not contain a required field', function() {
-
-  //   var stubMsg = {
-  //     username: 'Jono',
-  //     text: 'Do my bidding!'
-  //   };
-
-  //   var req = new stubs.request('/classes/messages', 'POST', stubMsg);
-  //   var res = new stubs.response();
-
-  //   handler.requestHandler(req, res);
-
-  //   expect(res._responseCode).to.equal(404);
-  //   expect(res._ended).to.equal(true);
-  // });
-
-
-  // it('Should 404 when asked for a nonexistent file', function() {
-  //   var req = new stubs.request('/arglebargle', 'GET');
-  //   var res = new stubs.response();
-
-  //   handler.requestHandler(req, res);
-
-  //   expect(res._responseCode).to.equal(404);
-  //   expect(res._ended).to.equal(true);
-  // });
-
-
-  // it('Should 404 when asked for a nonexistent file', function() {
-  //   var req = new stubs.request('/arglebargle', 'GET');
-  //   var res = new stubs.response();
-
-  //   handler.requestHandler(req, res);
-
-  //   expect(res._responseCode).to.equal(404);
-  //   expect(res._ended).to.equal(true);
-  // });
-
-
 
 });
